5410c1cd2ed214922712a1d3f5210e239dc0adec
Katelyn McCarthy <114868588+katelynmccarthy@users.noreply.github.com>
2022-11-30 13:01:12 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by <a href="https://katelyn-mccarthy-portfolio.netlify.app/" target="blank">Katelyn McCarthy</a> and is{" "}
@@ -12,6 +14,7 @@ export default function App() {
          open-sourced on Github
       </a>
       </footer>
+      </div>
     </div>
   );
 }
