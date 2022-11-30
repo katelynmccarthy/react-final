efd290dfa1c1eb4a6d67cad942c6480b568d2b89
Katelyn McCarthy <114868588+katelynmccarthy@users.noreply.github.com>
2022-11-30 12:41:29 -0500

    Added the github repo url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://katelyn-mccarthy-portfolio.netlify.app/" target="blank">Katelyn McCarthy</a> and is{" "}
+        <a href="https://github.com/katelynmccarthy/react-final"
+      target="_blank">
+         open-sourced on Github
+      </a>
+      </footer>
     </div>
   );
 }
