ba5ec3a139c0662e1678d7075e1d7bf5919c098b
Katelyn McCarthy <114868588+katelynmccarthy@users.noreply.github.com>
2022-12-01 17:34:37 -0500

    Fixed time and date showing

--- src/Weather.js
+++ src/Weather.js
@@ -6,43 +6,63 @@ import { useState } from "react";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ready:false});
+    const [city, setCity] = useState(props.defaultCity);
     function handleResponse(response) {
         setWeatherData({
             ready: true,
             temperature: response.data.temperature.current,
             humidity: response.data.temperature.humidity,
-            date: new Date(response.data.dt * 1000),
+            date: new Date(response.data.time * 1000),
             wind: response.data.wind.speed,
             city: response.data.name,
             description: response.data.condition.description,
-            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-        });
+            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+          });
     }
 
-if (weatherData.ready) {
-   return  <div className="Weather">
-    <form>
-        <div className="row">
-            <div className="col-9">
-    <input type="search" placeholder="Enter a city..."
-    className="form-control"
-    autoFocus="on"/>
-    </div>
-    <div className="col-3">
-    <input type="submit" value="Search"
-    className="btn btn-primary w-100"/>
-    </div>
-    </div>
-    </form>
-    <WeatherInfo data={weatherData} />
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+      }
     
-</div>
-;
-} else {
-    const apiKey = "7106ea97b4b3928fe085f9a75bao0f8t";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(handleResponse);
+      function handleCityChange(event) {
+        setCity(event.target.value);
+      }
 
+      function search() {
+        const apiKey = "7106ea97b4b3928fe085f9a75bao0f8t";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=imperial`;
+        axios.get(apiUrl).then(handleResponse);
+      }
+
+    if (weatherData.ready) {
+        return (
+          <div className="Weather">
+            <form onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Enter a city.."
+                    className="form-control"
+                    autoFocus="on"
+                    onChange={handleCityChange}
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary w-100"
+                  />
+                </div>
+              </div>
+            </form>
+            <WeatherInfo data={weatherData} />
+          </div>
+        ); 
+        } else {
+            search();
     return "Loading...";
 }
 }
